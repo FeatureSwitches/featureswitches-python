951425d05b16a18e3fc2ef83c3f027c66d8b3305
Joel Weirauch <joel@weirau.ch>
2016-03-28 11:53:18 -0500

    allow disabling local cache, logic consolidation, version bump

--- featureswitches/featureswitches.py
+++ featureswitches/featureswitches.py
@@ -32,11 +32,12 @@ class FeatureSwitches(object):
 
         self._cache = cache_factory()
 
-        self.sync()
+        if self._cache_timeout > 0:
+            self.sync()
 
-        t = threading.Thread(target=self._dirty_check)
-        t.setDaemon(True)
-        t.start()
+            t = threading.Thread(target=self._dirty_check)
+            t.setDaemon(True)
+            t.start()
 
     def authenticate(self):
         endpoint = 'authenticate'
@@ -47,23 +48,24 @@ class FeatureSwitches(object):
 
     def sync(self, force=False):
         endpoint = 'features'
-        r = self._http.get(endpoint)
-        if r:
-            self._last_feature_update = r.get('last_update')
-            self._last_sync = self._current_timestamp()
+        if self._cache_timeout > 0:
+            r = self._http.get(endpoint)
+            if r:
+                self._last_feature_update = r.get('last_update')
+                self._last_sync = self._current_timestamp()
 
-            for feature in r.get('features'):
-                feature_key = feature.get('feature_key', None)
+                for feature in r.get('features'):
+                    feature_key = feature.get('feature_key', None)
 
-                f = Feature(
-                        key=feature_key,
-                        enabled=feature.get('enabled'),
-                        include_users=feature.get('include_users', []),
-                        exclude_users=feature.get('exclude_users', []),
-                        last_sync=self._last_sync
-                )
+                    f = Feature(
+                            key=feature_key,
+                            enabled=feature.get('enabled'),
+                            include_users=feature.get('include_users', []),
+                            exclude_users=feature.get('exclude_users', []),
+                            last_sync=self._last_sync
+                    )
 
-                self._cache.set(feature_key, f)
+                    self._cache.set(feature_key, f)
 
 
     def add_user(self, user_identifier, customer_identifier=None, name=None, email=None):
@@ -83,34 +85,43 @@ class FeatureSwitches(object):
 
 
     def is_enabled(self, feature_key, user_identifier=None, default=False):
-        feature = self._cache.get(feature_key, ignore_expiration=True)
+        try:
+            feature = self._cache.get(feature_key, ignore_expiration=True)
+        except ValueError:
+            feature = None
 
         if feature and not self._cache_is_stale(feature):
-            if feature.enabled:
-                if feature.include_users:
-                    if user_identifier in feature.include_users:
-                        return True
-                    else:
-                        return False
-
-                if feature.exclude_users:
-                    if user_identifier in feature.exclude_users:
-                        return False
-
-                if not user_identifier and (feature.include_users or feature.exclude_users):
-                    return False
-
-                return feature.enabled
-            return feature.enabled
+            return self._enabled_for_user(feature, user_identifier)
         else:
             feature = self._get_feature(feature_key, user_identifier)
 
             if feature:
-                return self.is_enabled(feature_key, user_identifier)
+                return self._enabled_for_user(feature, user_identifier)
 
         return default
 
+    def _enabled_for_user(self, feature, user_identifier):
+        if feature.enabled:
+            if feature.include_users:
+                if user_identifier in feature.include_users:
+                    return True
+                else:
+                    return False
+
+            if feature.exclude_users:
+                if user_identifier in feature.exclude_users:
+                    return False
+
+            if not user_identifier and (feature.include_users or feature.exclude_users):
+                return False
+
+            return feature.enabled
+        return feature.enabled
+
     def _cache_is_stale(self, feature):
+        if self._cache_timeout == 0:
+            return True
+
         cache_expiration = self._current_timestamp() - self._cache_timeout
 
         if feature.last_sync > cache_expiration and self._last_dirty_check > cache_expiration:
@@ -140,7 +151,8 @@ class FeatureSwitches(object):
                     last_sync=self._current_timestamp()
             )
 
-            self._cache.set(feature_key, feature)
+            if self._cache_timeout > 0:
+                self._cache.set(feature_key, feature)
 
             return feature
 
